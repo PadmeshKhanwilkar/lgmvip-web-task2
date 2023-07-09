14032ad4482696975483659fb2c5af89499443c1
Padmesh Khanwilkar <0701cs201042@uecu.ac.in>
2023-07-09 16:20:37 +0530

    first commit

---
+++ src/components/card.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import fetchUsersfromAPI from "./FetchAPI";
+
+const Users = () => {
+  const [loading, setLoading] = useState(false); // State to track loading status
+  const [userData, setUserData] = useState([]); // State to store fetched user data
+
+  const fetchData = async () => {
+    setLoading(true); // Set loading to true before fetching data
+    try {
+      const users = await fetchUsersfromAPI(); // Fetch user data from the API
+      setUserData(users); // Update the user data state
+      // setTimeout(() => {
+      //   setLoading(false); // Set loading to false after a delay
+      // }, 1000); // Delay in milliseconds (e.g., 1000ms = 1 second)
+
+      setTimeout(() => {
+        setLoading(false); // Set loading to false after a delay
+      }, 2000); // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+    } catch (error) {
+      console.error(error);
+      setLoading(false); // Set loading to false if an error occurs
+    }
+  };
+
+  useEffect(() => {
+    setUserData([]); // Clear the user data when the component mounts or reloads
+  }, []);
+
+  return (
+    <div>
+      <div className="my-api">My API</div>
+      <p className="created-by">created by : Padmesh Khanwilkar</p>
+      <button className="get-users" onClick={fetchData}>
+        Get Users
+      </button>
+      {loading ? ( // Conditional rendering based on the loading state
+        <div className="loader"></div>
+      ) : (
+        <div>
+          {userData.map((user) => (
+            <div className="user-item" key={user.id}>
+              <img src={user.avatar} alt={user.avatar} className="avatar" />
+              <div className="right-section">
+                <h1>
+                  {user.first_name} {user.last_name}
+                </h1>
+                <p>ID: {user.id}</p>
+                <p>{user.email}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Users;
